40149f80bb816ec737891a1fa376096cd7a07571
pswang <pengshengwang@gmail.com>
2016-03-08 14:51:41 +0800

    update

--- utils.go
+++ utils.go
@@ -39,6 +39,13 @@ func Must(err error) (ok bool) {
 	return true
 }
 
+func LogErr(err error) error {
+	if err != nil {
+		log.Printf("%#v", err)
+	}
+	return err
+}
+
 func IfElse(is bool, If, Else interface{}) interface{} {
 	if is {
 		return If
