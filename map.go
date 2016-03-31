84325f0b929a5c865ea80a801ebdc215ff4d6784
pswang <pengshengwang@gmail.com>
2016-04-01 00:17:48 +0800

    update gobreak.map

--- map.go
+++ map.go
@@ -1,5 +1,9 @@
 package gobreak
 
+import (
+	"log"
+)
+
 type M map[string]T
 
 func (p M) HasKey(k string) bool {
@@ -16,7 +20,7 @@ func (p M) GetStr(k string) string { return p.GetStrOr(k, "") }
 
 func (p M) GetIntOr(k string, or int) int {
 	if v, ok := p[k]; ok {
-		return v.(int)
+		return getValAsInt(v)
 	}
 	return or
 }
@@ -24,7 +28,7 @@ func (p M) GetInt(k string) int { return p.GetIntOr(k, 0) }
 
 func (p M) GetF64Or(k string, or float64) float64 {
 	if v, ok := p[k]; ok {
-		return v.(float64)
+		return getValAsF64(v)
 	}
 	return or
 }
@@ -37,3 +41,27 @@ func (p M) GetBoolOr(k string, or bool) bool {
 	return or
 }
 func (p M) GetBool(k string) bool { return p.GetBoolOr(k, false) }
+
+func getValAsInt(v T) int {
+	switch t := v.(type) {
+	case int:
+		return v.(int)
+	case float64:
+		return int(v.(float64))
+	default:
+		log.Println("%v is %v", v, t)
+		return 0
+	}
+}
+
+func getValAsF64(v T) float64 {
+	switch t := v.(type) {
+	case int:
+		return float64(v.(int))
+	case float64:
+		return v.(float64)
+	default:
+		log.Println("%v is %v", v, t)
+		return 0
+	}
+}
