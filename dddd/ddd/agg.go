4cbfaa821d31629953b0a36369442c599d05a866
pswang <pengshengwang@gmail.com>
2016-04-23 17:31:38 +0800

    update

--- dddd/ddd/agg.go
+++ dddd/ddd/agg.go
@@ -19,10 +19,7 @@ type AggBase struct {
 	uncommitted []Event
 }
 
-func (p *AggBase) ID() GUID            { return p.Root().ID() }
-func (p *AggBase) Root() Entity        { return nil }
-func (p *AggBase) HandleCmd(Cmd) error { return nil }
-
+func (p *AggBase) HandleCmd(Cmd) error           { return nil }
 func (a *AggBase) StoreEvent(event Event)        { a.uncommitted = append(a.uncommitted, event) }
 func (a *AggBase) GetUncommittedEvents() []Event { return a.uncommitted }
 func (a *AggBase) HasUncommittedEvents() bool    { return len(a.uncommitted) > 0 }
