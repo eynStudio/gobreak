57deed333d9270006b469f72c1a5920bdf808fac
pswang <pengshengwang@gmail.com>
2016-05-03 00:06:16 +0800

    add msg_status

---
+++ msg_status.go
@@ -0,0 +1,18 @@
+package gobreak
+
+var OkStatus MsgStatus = MsgStatus{Status: "ok"}
+
+type MsgStatus struct {
+	Msg    string
+	Status string
+}
+
+func (p *MsgStatus) Err(msg string) *MsgStatus   { return p.SetMsg("err", msg) }
+func (p *MsgStatus) OkMsg(msg string) *MsgStatus { return p.SetMsg("ok", msg) }
+func (p *MsgStatus) Ok() *MsgStatus              { return p.SetMsg("ok", "") }
+
+func (p *MsgStatus) SetMsg(status, msg string) *MsgStatus {
+	p.Status = status
+	p.Msg = msg
+	return p
+}
