b27d86c80344a335615e98e03647359fd41424c9
pswang <pengshengwang@gmail.com>
2016-05-06 21:11:06 +0800

    add datelog

---
+++ log/datelog/log.go
@@ -0,0 +1,59 @@
+package datelog
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	. "github.com/eynstudio/gobreak"
+)
+
+type DateLog struct {
+	Error
+	outPath string
+	curDate time.Time
+	file    *os.File
+}
+
+func New(path string) *DateLog {
+	l := &DateLog{outPath: path, curDate: time.Now()}
+	l.openFile()
+	return l
+}
+
+func (p *DateLog) Log(v ...interface{}) {
+	p.checkFile()
+	fmt.Fprint(p.file, v...)
+}
+
+func (p *DateLog) Logf(format string, v ...interface{}) {
+	p.checkFile()
+	fmt.Fprintf(p.file, format, v...)
+}
+
+func (p *DateLog) Logln(v ...interface{}) {
+	p.checkFile()
+	fmt.Fprintln(p.file, v...)
+}
+
+func (p *DateLog) checkFile() {
+	if p.curDate.Day() != time.Now().Day() {
+		p.Close()
+		p.curDate = time.Now()
+		p.openFile()
+	}
+}
+
+func (p *DateLog) getFileName() string {
+	return fmt.Sprintf("%s/%s.log", p.curDate.Format("20060102"))
+}
+
+func (p *DateLog) openFile() {
+	p.file, p.Err = os.OpenFile(p.getFileName(), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0664)
+}
+
+func (p *DateLog) Close() {
+	if p.file != nil {
+		p.file.Close()
+	}
+}
