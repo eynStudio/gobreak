ec1d31a707b493c6593c9adfd900e50cdcae79b9
pswang <pengshengwang@gmail.com>
2016-06-23 14:02:30 +0800

    add

---
+++ uuid.go
@@ -0,0 +1,10 @@
+package gobreak
+
+import (
+	"github.com/satori/go.uuid"
+)
+
+func Uuid1() uuid.UUID                          { return uuid.NewV1() }
+func Uuid3(ns uuid.UUID, name string) uuid.UUID { return uuid.NewV3(ns, name) }
+func Uuid4() uuid.UUID                          { return uuid.NewV4() }
+func Uuid5(ns uuid.UUID, name string) uuid.UUID { return uuid.NewV5(ns, name) }
