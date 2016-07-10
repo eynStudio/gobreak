3564d7978d880c0f2b89b28ef0b6b0b0fc1543dc
pswang <pengshengwang@gmail.com>
2016-07-10 10:18:40 +0800

    update

--- orm/utils.go
+++ orm/utils.go
@@ -7,17 +7,35 @@ import (
 )
 
 func Extend(dest, src T) T {
-	destModel :=getModelInfo(dest)
+	destModel := getModelInfo(dest)
 	destVal := reflect.Indirect(reflect.ValueOf(dest))
 	srcVal := reflect.Indirect(reflect.ValueOf(src))
 	for k := range destModel.Fields {
-		if val := srcVal.FieldByName(k);val.IsValid(){
+		if val := srcVal.FieldByName(k); val.IsValid() {
 			destVal.FieldByName(k).Set(val)
 		}
 	}
 	return dest
 }
 
-func Map(dest,src T) T{
-	return Extend(dest,src)
+func Map(dest, src T) T {
+	return Extend(dest, src)
+}
+
+type IMapper interface {
+	Go2Db(n string) string
+	Db2Go(n string) string
+}
+
+type DbMapper struct {
+}
+
+func (p DbMapper) Go2Db(n string) string {
+
+	return ""
+}
+
+func (p DbMapper) Db2Go(n string) string {
+
+	return ""
 }
